d690e6ed7944453bc967f14b52db51723f427f43
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2024-02-01 22:17:44 +0530

    ngOnint

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,17 +1,25 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnChanges {
-
-  @Input() data?: number;
-
+export class ChildComponent implements OnChanges ,OnInit {
+  constructor() {
+    console.log('child constructor is called');
+    console.log(this.data);
+    console.log(this.name);
+}
+  @Input() data?: number = 232;
+  name: string = 'sreeraj'
+  @ViewChild('temp') para?: ElementRef ;
   ngOnChanges(changes: SimpleChanges): void {
     console.log(changes);
   }
-  
+   ngOnInit(): void {
+     console.log('child ngOninit called');
+console.log(this.para);
+   }
   
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor() {
+    console.log('Parent constructor is called');
+  }
   parent: number = 0
   
   buttonClick() {
     this.parent++;
   }
-
+ngOnInit(): void {
+  console.log('parent ngOninit is called');
+}
 }
